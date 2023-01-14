2eee1e78c2103db9da3cba8bc7c4cbbfa0301490
Swibm <swibm9009@gmalil.com>
2023-01-14 18:56:39 +0600

    files created

---
+++ Task_1/app.js
@@ -0,0 +1,46 @@
+﻿// 1) Используя регулярные выражения необходимо сделать поле для ввода эл почты и пароля
+
+const regExpMail = /^\w+@\w+\.com|\.ru|\.info$/
+const regExpPassword = /^(?=.*\d)(?=.*[a-z])/
+
+const mailInput = document.querySelector("#username-field")
+const passwordInput = document.querySelector("#password-field")
+const submitButton = document.querySelector("#button-logging")
+const hidePassword = document.querySelector(".hide_pass")
+
+function mailCheck(str) {
+    if(regExpMail.test(str) && str.length <=25)
+    {return true}
+    else {return false}
+}
+
+function passwordCheck(str) {
+    if(regExpPassword.test(str) && str.length <=20 && str.length >=4)
+    {return true}
+    else {return false}
+}
+
+
+submitButton.addEventListener("click", () => {
+    if(mailCheck(mailInput.value) && passwordCheck(passwordInput.value))
+    {
+        alert("correct credentials")
+    }
+    else 
+    {
+        alert("incorrect credentials")  
+    }
+})
+
+hidePassword.addEventListener("click", () => {
+    if(passwordInput.getAttribute('type') == "password")
+    {
+        passwordInput.setAttribute('type','text')
+        hidePassword.setAttribute('src', 'pics/show_pass.png')
+    }
+    else 
+    {
+        passwordInput.setAttribute('type','password')
+        hidePassword.setAttribute('src', 'pics/hide_pass.png')
+    }
+})
---
+++ Task_2/app.js
@@ -0,0 +1,17 @@
+﻿// 2) используя рекурсию необходимо заставить блок двигаться по странице. Создайте один большой блок и один маленький внутри него. 
+// Задайте большому блоку position: relative, а малому absolute. Используя addEventListener изменяйте параметр позиции малого блока 
+// (.style.left=`${переменная}px`). 
+
+let change = 0
+const elementToMove = document.querySelector('.the-small-one')
+
+let positionChange = () => {
+    change += 10
+    elementToMove.style.left = `${change}px`
+    elementToMove.style.top = `${change}px`
+    if(change < 800) {
+        positionChange()
+    }
+}
+
+elementToMove.addEventListener('click',positionChange)
